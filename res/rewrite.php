d3e31c729b62a7ea54100df250538f322a0af815
PiotrPress <admin@piotr.press>
2023-01-09 00:10:09 +0100

    v1.0.1

--- res/rewrite.php
+++ res/rewrite.php
@@ -1,6 +1,6 @@
 <?php declare( strict_types = 1 );
 
-require_once __DIR__ . '/vendor/autoload.php';
+require_once dirname( __DIR__ ) . '/src/Router.php';
 
 if ( $rewrite = PiotrPress\WordPress\Router::rewrite(
     $_SERVER[ 'DOCUMENT_ROOT' ],
